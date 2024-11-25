78b020e4441ea7c1eefa73e911f8b0ef5c386156
Falsidical <franport@outlook.com>
2024-11-25 21:26:50 +0000

    Negatives Done

--- calculator.js
+++ calculator.js
@@ -4,50 +4,21 @@ const opButtons = document.querySelectorAll('button[data-op]');
 const screenResult = document.querySelector('#number');
 const screenExpression = document.querySelector('#history');
 
-let minusUsed = false;
-let result = 0;
-let expression = '';
-
-function updateScreen() {
-  screenResult.innerText = result;
-  screenExpression.innerText = expression;
-}
-
-function insertText(key) {
-  if (expression.length && !minusUsed && (isNaN(expression.slice(-1)) || isNaN(key.target.dataset.key))) {
-    expression += ' ';
-  }
-  expression += key.target.dataset.key;
-  minusUsed = false;
-  updateScreen();
-  console.log(expression);
-}
-
-inputButtons.forEach((btn) => btn.addEventListener('click', insertText));
+inputButtons.forEach((btn) =>
+  btn.addEventListener('click', (e) => {
+    screenHistory.innerText += e.target.dataset.key;
+  })
+);
 
 opButtons[0].addEventListener('click', () => {
-  expression += '-';
-  minusUsed = true;
-  updateScreen();
-  console.log(expression);
+  screenHistory.innerText = screenHistory.innerText.slice(0, -1);
 });
 
 opButtons[1].addEventListener('click', () => {
-  expression = expression.slice(0, -1);
-  if (expression.slice(-1) === ' ') expression = expression.slice(0, -1);
-  updateScreen();
-  console.log(expression);
+  screenHistory.innerText = '';
+  screenResult.innerText = '';
 });
 
-opButtons[2].addEventListener('click', () => {
-  expression = '';
-  result = '0';
-  updateScreen();
-  console.log(expression);
-});
-
-opButtons[4].addEventListener('click', () => {
-  result = calculate(expression);
-  updateScreen();
-  console.log(expression);
+opButtons[3].addEventListener('click', () => {
+  screenResult.innerText = calculate(screenHistory.innerText);
 });
